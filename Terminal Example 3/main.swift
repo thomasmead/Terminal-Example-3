7f82668d8829373a6b7c58d3e67661b9e387654f
Thomas Mead <thomas.d.mead@gmail.com>
2021-02-13 12:21:33 -0500

    from MacBook 20210213

--- Terminal Example 3/main.swift
+++ Terminal Example 3/main.swift
@@ -15,11 +15,12 @@ var test = testClass("name")
 try test.file.parseWorkbooks()
 var workbooks = try test.file.parseWorkbooks()
 var paths = try test.file.parseWorksheetPaths()
-var worksheet = try test.file.parseWorksheet(at: paths[6])
+var worksheet = try test.file.parseWorksheet(at: paths[4])
 
 
 var workbook = workbooks[0]
-var worksheets = workbook.sheets
+var worksheets = workbook.sheets.items
+//var worksheet = worksheets[0]
 var rows = worksheet.data?.rows
 var row = rows?[1]
 var cells = row?.cells
@@ -27,7 +28,7 @@ var cell = cells?[1]
 var data = cell?.value
 var cellArray : [Cell]
 row = rows?[3]
-cells = row?.cells
+//cells = row?.cells
 
 cellArray = row!.cells
 
@@ -35,16 +36,23 @@ var testString = cellArray[1].value
 testString = cellArray[1].value
 testString = cellArray[4].value
 
-for ix in cellArray{
-    print(ix.value)
-    var temp = ix.inlineString
-    var type = ix.type
-    var item = ix.value
-    print(type)
-    print(item!)
-    print(temp)
+print(worksheet.data?.rows)
+for i in rows!{
+    var count = 1
+    row = i
+    cellArray = row!.cells
+    for ix in 1...cellArray.count-1{
+       var item = cellArray[ix].value
+        if item != nil{
+            print("row \(count) - cell \(ix) -- \(item!)")
+        }
+        else{
+            print("\(ix) was nil")
+        }
+        
+    }
+    count += 1
 }
-
 
 print(paths[1])
 print(data!)
